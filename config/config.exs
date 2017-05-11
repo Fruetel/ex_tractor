65abf6b732da5bf39d42a1a0e461bd8b868ffbb7
Thomas Fruetel <github@fruetel.de>
2017-05-11 18:24:36 +0200

    Add config for Publisher

--- config/config.exs
+++ config/config.exs
@@ -6,4 +6,9 @@ config :ex_tractor, Consumer,
   routing_key: System.get_env("INCOMING_ROUTING_KEY") || "unprocessed",
   service: System.get_env("SERVICE_NAME") || "ex_tractor"
 
+config :ex_tractor, Publisher,
+  url: System.get_env("RABBITMQ_URL") || "amqp://localhost",
+  exchange: System.get_env("OUTGOING_EXCHANGE") || "urls",
+  routing_key: System.get_env("OUTGOING_ROUTING_KEY") || "extracted"
+
 #     import_config "#{Mix.env}.exs"
